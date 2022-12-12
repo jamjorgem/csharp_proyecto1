4a7a4fe5eda34dbb81e9e55ddb03552d41bec788
jorgeandres-oktal <50455838+jorgeandres-oktal@users.noreply.github.com>
2022-12-12 12:44:30 -0500

    first test

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Escribe tu nombre");
+string nombre = Console.ReadLine();
+Console.WriteLine("Tu nombre es:" + nombre);
+string horaActual = DateTime.Now.ToString("hh:mm:ss");
+Console.WriteLine("La hora actual es "+ horaActual);
